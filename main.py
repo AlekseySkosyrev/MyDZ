a3c1c4c33d860ecb0ff0842bebf9451e54be86f2
Алексей Скосырев <skosyrevalexey.as@gmail.com>
2024-09-26 08:22:22 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9**0.5*5)
+
+# 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3st program
+print(2*2+2)
+print(2*(2+2))
+print((2*(2+2))==(2*2+2))
+
+# 4st program
+a='123.456'
+print(a)
+x=(float('123.456')*10)
+x1=(x % 10)
+x2=(x1 // 1)
+print(int(x2))
+
