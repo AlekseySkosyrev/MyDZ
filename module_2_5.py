916e67890bdd7643f5890b8b8c95c7875aba4c80
Алексей Скосырев <skosyrevalexey.as@gmail.com>
2024-09-26 06:58:53 +0400

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -7,16 +7,12 @@ def get_matrix(n, m, value):
     print(matrix)
     return matrix
 
-n = int(input('Сколько строк матрицы?'))
-m = int(input('Сколько столбцов матрицы?'))
+n = int(input('Задайте количество строк матрицы    :'))
+m = int(input('Задайте количество столбцов матрицы :'))
 value = input(f'Задайте значения матрицы : ')
-print('-------' * m)
+
 matrix = get_matrix(n, m, value)
-if n <= 0:
-    print("Матрица пуста, задано неверное количество строк:", n)
-elif m <=0:
-    print("Матрица пуста, задано неверное количество столбцов:" ,m)
-else:
-    print("Матрица воплоти:")
-    for i in matrix:
-        print(*i)
+print()
+
+for i in matrix:
+    print(*i)
